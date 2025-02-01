29e927da84a9d746da10a50001a2efe0f82bb431
Wahyu Nur Salam <wahyurhythm@gmail.com>
2025-02-01 11:14:03 +0700

    product route

--- application/controllers/ProductController.php
+++ application/controllers/ProductController.php
@@ -1,10 +1,63 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class ProductController extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('welcome_message');
+
+
+		$header['title'] = 'Product';
+        $this->load->view('_layout/header', $header);
+		$this->load->view('product/index');
+        $this->load->view('_layout/footer');
+	}
+
+	public function show($id)
+	{
+		
+
+		$header['title'] = 'Detail Product';
+        $this->load->view('_layout/header', $header);
+		$this->load->view('product/show');
+        $this->load->view('_layout/footer');
+	}
+
+	public function create()
+	{
+		if ($this->input->server('REQUEST_METHOD') === 'GET') {
+
+			$header['title'] = 'Create Product';
+			$this->load->view('_layout/header', $header);
+			$this->load->view('product/create');
+			$this->load->view('_layout/footer');
+
+		}elseif($this->input->server('REQUEST_METHOD') === 'POST'){
+
+		}
+	}
+
+	public function edit($id)
+	{
+
+
+		$header['title'] = 'Edit Product';
+		$this->load->view('_layout/header', $header);
+		$this->load->view('product/edit');
+		$this->load->view('_layout/footer');
+	}
+
+	public function update($id)
+	{
+		if($this->input->server('REQUEST_METHOD') === 'POST'){
+
+		}
+	}
+
+	public function delete($id)
+	{
+		if($this->input->server('REQUEST_METHOD') === 'POST'){
+
+		}
 	}
 }
